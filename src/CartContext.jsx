806185c3d1f92fb42a97ecfbf5c9aa38aa6f1023
Divya-BA <137373896+Divya-BA@users.noreply.github.com>
2023-08-27 20:52:49 +0530

    CartContext.jsx

--- src/CartContext.jsx
+++ src/CartContext.jsx
@@ -1,4 +1,4 @@
-// CartContext.js
+
 import React, { createContext, useContext, useReducer } from 'react';
 
 const CartContext = createContext();
@@ -7,17 +7,16 @@ const initialState = {
   cartItems: [],
 };
 
-// CartContext.js
-// ... (other code)
+
 
 const cartReducer = (state, action) => {
     switch (action.type) {
       case 'ADD_TO_CART':
-        // Check if the product is already in the cart
+    
         const existingItem = state.cartItems.find(item => item.id === action.payload.id);
   
         if (existingItem) {
-          // If the product exists, update its quantity
+          
           const updatedCartItems = state.cartItems.map(item =>
             item.id === action.payload.id
               ? { ...item, quantity: item.quantity + 1 }
@@ -29,7 +28,7 @@ const cartReducer = (state, action) => {
             cartItems: updatedCartItems,
           };
         } else {
-          // If the product doesn't exist, add it to the cart with quantity 1
+
           return {
             ...state,
             cartItems: [...state.cartItems, { ...action.payload, quantity: 1 }],
@@ -58,14 +57,13 @@ const cartReducer = (state, action) => {
           cartItems: state.cartItems.filter(item => item.id !== action.payload),
         };
   
-      // ... (other cases)
+
   
       default:
         return state;
     }
   };
-  
-  // ... (other code)
+
   
 
 export const CartProvider = ({ children }) => {
